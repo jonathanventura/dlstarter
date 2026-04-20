65b504ac1e6760748c12d05cceca384c7547c40d
Jonathan Ventura <jventu09@calpoly.edu>
2026-04-19 21:57:39 -0700

    updating autoencoder trainer

--- src/dlstarter/model.py
+++ src/dlstarter/model.py
@@ -12,10 +12,41 @@ class Trainer:
         self.metric = metric.to(device)
         self.device = device
 
+    def evaluate(self, dl, verbose=False):
+        self.model.eval()
+        self.metric.reset()
+        if verbose:
+            batches = tqdm.tqdm(dl,desc='evaluate')
+        else:
+            batches = dl
+        for batch in batches:
+            x_batch, y_batch = batch
+            x_batch = x_batch.to(self.device)
+            y_batch = y_batch.to(self.device)
+            y_pred = self.model(x_batch)
+            self.metric(y_pred,y_batch)
+        return self.metric.compute().item()        
+    
+    def predict(self, dl, verbose=False):
+        self.model.eval()
+        self.metric.reset()
+        if verbose:
+            batches = tqdm.tqdm(dl,desc='predict')
+        else:
+            batches = dl
+        preds = []
+        for batch in batches:
+            x_batch, _ = batch
+            x_batch = x_batch.to(self.device)
+            y_pred = self.model(x_batch)
+            preds.append(y_pred.detach().cpu().numpy())
+        return np.stack(preds)
+
     def fit(self, dl_train, optimizer, dl_val=None, num_epochs=10, checkpoint_path=None, verbose=False):
         train_metrics = []
         val_metrics = []
         for epoch in range(num_epochs):
+            self.model.train()
             self.metric.reset()
             if verbose:
                 batches = tqdm.tqdm(dl_train,desc=f'epoch {epoch+1}/{num_epochs}')
@@ -48,18 +79,7 @@ class Trainer:
                     print(f'Epoch {epoch}: train {train_metric}')
             else:
                 # compute metric on validation set
-                self.metric.reset()
-                if verbose:
-                    batches = tqdm.tqdm(dl_val,desc='validation')
-                else:
-                    batches = dl_val
-                for batch in batches:
-                    x_batch, y_batch = batch
-                    x_batch = x_batch.to(self.device)
-                    y_batch = y_batch.to(self.device)
-                    y_pred = self.model(x_batch)
-                    self.metric(y_pred,y_batch)
-                val_metric = self.metric.compute().item()
+                val_metric = self.evaluate(dl_val,verbose=verbose)
                 if checkpoint_path is not None:
                     do_save = False
                     if len(val_metrics) == 0:
@@ -115,25 +135,43 @@ class AutoencoderTrainer:
             self.metric = MeanSquaredError().to(device)
         self.device = device
         self.activity_reg = activity_reg
-
+        
+    def encode(self, dl, verbose=False):
+        self.encoder.eval()
+        if verbose:
+            batches = tqdm.tqdm(dl,desc='predict')
+        else:
+            batches = dl
+        preds = []
+        for batch in batches:
+            x_batch, _ = batch
+            x_batch = x_batch.to(self.device)
+            y_pred = self.encoder(x_batch)
+            preds.append(y_pred.detach().cpu().numpy())
+        return np.stack(preds)
+    
     def fit(self, dl_train, optimizer, dl_val=None, num_epochs=10, verbose=False):
         train_metrics = []
         val_metrics = []
         for epoch in range(num_epochs):
+            self.encoder.train()
+            self.decoder.train()
             self.metric.reset()
             if verbose:
                 batches = tqdm.tqdm(dl_train,desc=f'epoch {epoch+1}/{num_epochs}')
             else:
                 batches = dl_train
-            for x_batch, y_batch in batches:
+            for x_batch in batches:
+                if isinstance(x_batch,tuple) or isinstance(x_batch,list):
+                    x_batch = x_batch[0]
+                
                 # move data to device
                 x_batch = x_batch.to(self.device)
-                y_batch = y_batch.to(self.device)
 
                 # compute model output and loss
                 h_pred = self.encoder(x_batch)
-                y_pred = self.decoder(h_pred)
-                loss = self.loss_fn(y_pred,y_batch)
+                x_pred = self.decoder(h_pred)
+                loss = self.loss_fn(x_pred,x_batch)
 
                 # compute activity regularization
                 if self.activity_reg > 0:
@@ -141,7 +179,7 @@ class AutoencoderTrainer:
                     loss += self.activity_reg * reg
 
                 # compute metric for train batch
-                self.metric(y_pred,y_batch)
+                self.metric(x_pred,x_batch)
 
                 # update weights
                 optimizer.zero_grad()
@@ -158,17 +196,19 @@ class AutoencoderTrainer:
                     print(f'Epoch {epoch}: train {train_metric}')
             else:
                 # compute metric on validation set
+                self.encoder.eval()
+                self.decoder.eval()
                 self.metric.reset()
                 if verbose:
                     batches = tqdm.tqdm(dl_val,desc='validation')
                 else:
                     batches = dl_val
-                for batch in batches:
-                    x_batch, y_batch = batch
+                for x_batch in batches:
+                    if isinstance(x_batch,tuple) or isinstance(x_batch,list):
+                        x_batch = x_batch[0]
                     x_batch = x_batch.to(self.device)
-                    y_batch = y_batch.to(self.device)
-                    y_pred = self.decoder(self.encoder(x_batch))
-                    self.metric(y_pred,y_batch)
+                    x_pred = self.decoder(self.encoder(x_batch))
+                    self.metric(x_pred,x_batch)
                 val_metric = self.metric.compute().item()                    
                 val_metrics.append(val_metric)
 
@@ -179,7 +219,6 @@ class AutoencoderTrainer:
             return train_metrics
         else:
             return train_metrics, val_metrics
-        
 
 def fit_model(
           model, opt, loss_fn, metric, device,
